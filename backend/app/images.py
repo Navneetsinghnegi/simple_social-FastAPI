b397a8fe6ec264db56ca434e72ce7000f7f522aa
Navneetsinghnegi <navneetsingh5647@gmail.com>
2025-12-14 17:08:58 +0530

    corrected imagekit instance for v5

--- backend/app/images.py
+++ backend/app/images.py
@@ -5,8 +5,4 @@ import os
 
 load_dotenv()
 
-imagekit = ImageKit(
-    os.getenv("IMAGEKIT_PRIVATE_KEY"),
-    os.getenv("IMAGEKIT_PUBLIC_KEY"),
-    os.getenv("IMAGEKIT_URL_ENDPOINT")
-)
+imagekit = ImageKit()

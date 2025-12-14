b4d78bda64ccd36776253cc824eaf7cf03cb6274
Navneetsinghnegi <navneetsingh5647@gmail.com>
2025-12-14 17:56:04 +0530

    fixed import for imagekit

--- backend/app/app.py
+++ backend/app/app.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
 from app.schemas import PostCreate, PostResponse, UserRead, UserCreate, UserUpdate
 from app.db import Post, create_db_and_tables, get_async_session, User
 from sqlalchemy.ext.asyncio import AsyncSession
-from imagekitio.models import UploadFileRequestOptions
+from imagekitio.models.UploadFileRequestOptions import UploadFileRequestOptions
 from contextlib import asynccontextmanager
 from sqlalchemy import select
 from app.images import imagekit
